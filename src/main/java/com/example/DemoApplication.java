84705b46827a347eb784b4cfc9ab9d2dc3633773
plusoneky <plusone@163.com>
2025-05-07 08:31:40 +0800

    commit

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -19,17 +19,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-
-		String input = "aaabbcddd";
-		String result = input.chars()
-				.mapToObj(c -> String.valueOf((char) c))
-				.collect(Collectors.joining()) // 先将字符转换为字符串流，然后合并为单个字符串
-				.chars()
-				.mapToObj(c -> String.valueOf((char) c))
-				.distinct() // 去重
-				.collect(Collectors.joining()); // 合并回字符串
-		System.out.println(result); // 输出 abcde
-
 	}
 
 }
